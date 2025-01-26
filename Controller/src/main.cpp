e3fc12cb37ded4c25b8628e2120e8aa121b994d0
DG-7D <96519093+DG-7D@users.noreply.github.com>
2025-01-26 15:50:20 +0900

    動きそう(有効期限設定してない)

--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -17,9 +17,9 @@ const uint8_t PIN_SERVO_ARM = 17;
 
 // サーボ設定
 const uint16_t MICROS_SERVO_STOP = 1500;
-const int16_t MICROS_SERVO_X_MOVE = 100;
-const int16_t MICROS_SERVO_Y_MOVE = -100;
-const uint16_t MICROS_SERVO_Z_MOVE = 100;
+const int16_t MICROS_SERVO_X_MOVE = 500;
+const int16_t MICROS_SERVO_Y_MOVE = -500;
+const uint16_t MICROS_SERVO_Z_MOVE = 500;
 const uint8_t DEGREE_SERVO_ARM_CLOSE_MIN = 0;
 const uint8_t DEGREE_SERVO_ARM_CLOSE_MAX = 5;
 const uint8_t DEGREE_SERVO_ARM_OPEN = 45;
@@ -59,6 +59,8 @@ void setup() {
     servoArm.attach(PIN_SERVO_ARM);
 
     execCommand("0,0,0,0");
+
+    Serial.print("> ");
 }
 
 void execCommand(String command) {
@@ -77,58 +79,69 @@ void execCommand(String command) {
 
     if (values[0] == -1 && digitalRead(PIN_LIMIT_SWITCH_X_START) != LOW) {
         servoX.writeMicroseconds(MICROS_SERVO_X_BACKWARD);
-        // Serial.print("X-, ");
+        Serial.print("X-, ");
     } else if (values[0] == 1 && digitalRead(PIN_LIMIT_SWITCH_X_END) != LOW) {
         servoX.writeMicroseconds(MICROS_SERVO_X_FORWARD);
-        // Serial.print("X+, ");
+        Serial.print("X+, ");
     } else {
         servoX.writeMicroseconds(MICROS_SERVO_STOP);
-        // Serial.print("X0, ");
+        Serial.print("X0, ");
     }
 
     if (values[1] == -1 && digitalRead(PIN_LIMIT_SWITCH_Y_START) != LOW) {
         servoY.writeMicroseconds(MICROS_SERVO_Y_BACKWARD);
-        // Serial.print("Y-, ");
+        Serial.print("Y-, ");
     } else if (values[1] == 1 && digitalRead(PIN_LIMIT_SWITCH_Y_END) != LOW) {
         servoY.writeMicroseconds(MICROS_SERVO_Y_FORWARD);
-        // Serial.print("Y+, ");
+        Serial.print("Y+, ");
     } else {
         servoY.writeMicroseconds(MICROS_SERVO_STOP);
-        // Serial.print("Y0, ");
+        Serial.print("Y0, ");
     }
 
     if (values[2] == -1) {
         servoZ.writeMicroseconds(MICROS_SERVO_Z_DOWN);
-        // Serial.print("Z-, ");
+        Serial.print("Z-, ");
     } else if (values[2] == 1 && digitalRead(PIN_LIMIT_SWITCH_Z_TOP) != LOW) {
         servoZ.writeMicroseconds(MICROS_SERVO_Z_UP);
-        // Serial.print("Z+, ");
+        Serial.print("Z+, ");
     } else {
         servoZ.writeMicroseconds(MICROS_SERVO_STOP);
-        // Serial.print("Z0, ");
+        Serial.print("Z0, ");
     }
 
     if (values[3] == 1) {
         servoArm.write(DEGREE_SERVO_ARM_CLOSE_MIN);
-        // Serial.print("AO, ");
+        Serial.print("AO, ");
     } else {
         servoArm.write(DEGREE_SERVO_ARM_OPEN);
-        // Serial.print("AC, ");
+        Serial.print("AC, ");
     }
 
-    // Serial.println();
-    while (Serial.available()) Serial.read();
+    Serial.println();
 }
 
+String command = "";
+
 void loop() {
-    String data = Serial.readString();
-    if (data) {
-        lastCommandTime = millis();
-        isCommandActive = true;
-        execCommand(data);
-    } else if (isCommandActive && millis() - lastCommandTime > COMMAND_LIFETIME) {
-        // Serial.println("Timeout");
-        isCommandActive = false;
-        execCommand("0,0,0,0");
+    if (Serial.available()) {
+        char input = Serial.read();
+        if (input == '\r') {
+            return;
+        }
+
+        Serial.println();
+
+        if (input == '\n') {
+            execCommand(command);
+            command = "";
+        } else if (input == '\b') {
+            command = command.substring(0, command.length() - 1);
+        } else {
+            command += input;
+        }
+
+        Serial.print("> ");
+        Serial.print(command);
     }
 }
