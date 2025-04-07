f3e53bb331313142f7e39555d1b1e7841dcb265d
DG-7D <96519093+DG-7D@users.noreply.github.com>
2025-04-07 17:35:42 +0900

    uno破壊

--- L293Driver/src/main.cpp
+++ L293Driver/src/main.cpp
@@ -3,18 +3,10 @@
 constexpr uint8_t motorCount = 4;
 constexpr uint8_t outputCount = 2 * motorCount;
 
-#ifdef __AVR_ATtinyX4__
 constexpr uint8_t pinArray[outputCount] = {PIN_PB2, PIN_PA4,
                                            PIN_PA7, PIN_PA5,
                                            PIN_PB0, PIN_PA0,
                                            PIN_PB1, PIN_PA3};
-
-#elif __AVR_ATmega328P__
-constexpr uint8_t pinArray[outputCount] = {2, 3,
-                                           4, 5,
-                                           6, 7,
-                                           8, 9};
-#endif
 
 constexpr uint8_t pulseWidthArray[outputCount] = {31, 31,
                                                   63, 63,
