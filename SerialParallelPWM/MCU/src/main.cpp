09ab35eaa95775106ad51f96e3bd56d7b94b6aa3
DG-7D <96519093+DG-7D@users.noreply.github.com>
2025-05-16 17:00:36 +0900

    ピン配置を新基板に合わせた

--- SerialParallelPWM/MCU/src/main.cpp
+++ SerialParallelPWM/MCU/src/main.cpp
@@ -5,14 +5,14 @@ constexpr uint8_t outputCount = 2 * motorCount;
 
 constexpr uint8_t portAvailable[2] = {0b11111001, 0b00000111}; // PA2(RXD), PA1(TXD), PB3(RESET)には触れない
 constexpr byte portMappingArray[outputCount][2] = {
-    {0, 1 << 2}, // PB2
+    {1 << 3, 0}, // PA3
     {1 << 4, 0}, // PA4
-    {1 << 7, 0}, // PA7
     {1 << 5, 0}, // PA5
-    {0, 1 << 0}, // PB0
-    {1 << 0, 0}, // PA0
+    {0, 1 << 2}, // PB2
+    {1 << 7, 0}, // PA7
+    {1 << 6, 0}, // PA6
     {0, 1 << 1}, // PB1
-    {1 << 3, 0}  // PA3
+    {0, 1 << 0}, // PB0
 };
 
 constexpr uint8_t pulseWidthArray[outputCount] = {31, 31,
